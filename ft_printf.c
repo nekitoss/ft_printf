ce0ebe089d51395e1efb7f58b9749f4ee1424867
Nick <nekit_p@ukr.net>
2017-03-27 21:07:24 +0300

    stdarg skeleton and testing strstr

--- ft_printf.c
+++ ft_printf.c
@@ -1,4 +1,15 @@
+#include "ft_printf.h"
 
+int		ft_printf(char *str, ...)
+{
+	va_list	ap;
 
+	va_start(ap, str);
 
-//return(number_of_sym_printed) or -1 if error
+	printf("try to find %%%%  =  \"%s\"\n", ft_strstr(str, "%%"));
+
+	//vartypevar = va_arg(ap, vartype);
+	va_end(ap);
+	return (0);
+	//return(number_of_sym_printed) or -1 if error
+}
