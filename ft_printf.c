d203938b3bc4863dd6f727e85ced3dd12fd79bec
Nick <nekit_p@ukr.net>
2017-03-28 21:19:53 +0300

    fuct for creating str of len fullfilled with char

--- ft_printf.c
+++ ft_printf.c
@@ -1,13 +1,18 @@
 #include "ft_printf.h"
 
+char	*ft_newstrnchar(size_t len, char c)
+{
+	return(((char *)ft_memset(ft_strnew(len), c, len)));
+}
+
 int		ft_printf(char *str, ...)
 {
 	va_list	ap;
 
 	va_start(ap, str);
 
-	printf("try to find %%%%  =  \"%s\"\n", ft_strstr(str, "%%"));
-
+	// printf("try to find %%%%  =  \"%s\"\n", ft_strstr(str, "%%"));
+	printf("%s\n", ft_newstrnchar(5, '*'));
 	//vartypevar = va_arg(ap, vartype);
 	va_end(ap);
 	return (0);
